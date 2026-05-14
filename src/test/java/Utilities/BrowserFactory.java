bdd784adf9933af6f4b2dc4ac20dfc3645770d5b
LincolnX2 <sabur.yinus@gmail.com>
2026-05-14 23:02:05 +0200

    create BrowserFactory code

--- src/test/java/Utilities/BrowserFactory.java
+++ src/test/java/Utilities/BrowserFactory.java
@@ -1,7 +1,34 @@
 package Utilities;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.safari.SafariDriver;
+
 public class BrowserFactory {
 
+    static WebDriver driver;
 
+    public static WebDriver startBrowser(String browserChoice, String url) {
 
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        if (browserChoice.equalsIgnoreCase("Chrome")) {
+            driver = new ChromeDriver(chromeOptions);
+            chromeOptions.addArguments("--headless");
+
+        } else if (browserChoice.equalsIgnoreCase("Edge")) {
+            EdgeOptions EdgeOptions = new EdgeOptions();
+            driver = new EdgeDriver(EdgeOptions);
+
+        } else {
+            driver = new SafariDriver();
+
+        }
+        driver.get(url);
+        driver.manage().window().maximize();
+        return driver;
+    }
 }
